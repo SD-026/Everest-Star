cfa8e7b0e068e802339ef1d4a66ea70ac6b60d7f
SD-026 <140894438+SD-026@users.noreply.github.com>
2025-04-20 11:17:38 +0500

    disable  commit

--- everststar/src/App.jsx
+++ everststar/src/App.jsx
@@ -18,9 +18,9 @@ function App() {
         <Navbar />
         <div className="container mx-auto px-4 py-8">
           <Routes>
-            <Route path="/" element={<EmployeeForm/>} />
+            {/* <Route path="/" element={<EmployeeForm/>} /> */}
             <Route path="/admin/login" element={<AdminLogin />} />
-            <Route path="/unauthorized" element={<Unauthorized />} />
+            <Route path="/" element={<Unauthorized />} />
 
             
 
